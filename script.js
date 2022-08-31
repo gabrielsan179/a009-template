3ff94688c6a11b78bedd257fdae471cda7ec481a
gabrielsan179 <gabrielsan179@gmail.com>
2022-08-31 12:01:59 -0300

    exercicios em aula

---
+++ script.js
@@ -0,0 +1,26 @@
+/*let nome = prompt("qual seu nome?")
+let cor = prompt("qual sua cor favorita?")
+console.log("A cor favorita de " + nome + " é " + cor)
+console.log(`A cor favorita de ${nome} é ${cor}`)
+let citacao = prompt("qual sua citação favorita?")
+console.log("A cor favorita de " + nome + " é " + cor + " e a citação favorita é \"" + citacao + "\".")
+console .log(`nome: ${nome} \nCor favorita: ${cor}`)
+let nomeMaiusculo = nome.toUpperCase()
+console.log("A cor favorita de " + nomeMaiusculo + " é " + cor + " e a citação favorita é \"" + citacao + "\".")
+console.log(`nome possui ${nome.length} caracteres.`)
+console.log(`nome possui a letra \"A\"? ${nomeMaiusculo.includes("A")}`)*/
+
+/*let nome = prompt("qual seu nome?")
+let email = prompt("qual seu e-mail?")
+let frase1 = `O e-mail ${email} foi cadastrato com sucesso.\nBoas vindas, ${nome}!`
+let frase2 = `O e-mail ${email} foi cadastrato com sucesso.\nBoas vindas, ${nome}! \no nome ${nome} possui ${nome.length} caracteres`
+console.log(frase1)
+console.log(frase2)
+console.log(frase2.replaceAll("r", "l"))
+console.log("o e-mail possui \"@\"?", email.includes("@"))*/
+
+
+
+
+
+
